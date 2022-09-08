5390366b78b732a61cd0f223b1d7c5aef9d3fdd6
so yeon <alicesysy@swu.ac.kr>
2022-09-08 11:05:39 +0900

    [#1] 프로젝트 폴더링

---
+++ NiCarNaeCar/NiCarNaeCar/Screen/Onboarding/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiCarNaeCar
+//
+//  Created by 소연 on 2022/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
