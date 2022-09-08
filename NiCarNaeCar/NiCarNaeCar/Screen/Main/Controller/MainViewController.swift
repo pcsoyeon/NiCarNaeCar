67ea7cef6ac3a08abcbdbe324496bba91e7fb8c2
so yeon <alicesysy@swu.ac.kr>
2022-09-08 11:31:12 +0900

    [#1] 시작 화면 변경

---
+++ NiCarNaeCar/NiCarNaeCar/Screen/Main/Controller/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  NiCarNaeCar
+//
+//  Created by 소연 on 2022/09/08.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
