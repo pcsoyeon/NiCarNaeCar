fec20aecc18bb066fb6260a6724dd1f907a8a99a
so yeon <alicesysy@swu.ac.kr>
2022-09-19 01:04:29 +0900

    [#30] 문의하기 버튼 구현

--- NiCarNaeCar/NiCarNaeCar/Screen/Setting/View/Controller/SettingViewController.swift
+++ NiCarNaeCar/NiCarNaeCar/Screen/Setting/View/Controller/SettingViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SafariServices
+import MessageUI
 
 import NiCarNaeCar_Util
 import NiCarNaeCar_Resource
@@ -84,6 +85,20 @@ final class SettingViewController: BaseViewController {
         let safariView: SFSafariViewController = SFSafariViewController(url: url as URL)
         transition(safariView, transitionStyle: .present)
     }
+    
+    private func sendMail() {
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            composeViewController.setToRecipients(["alice9809@naver.com"])
+            composeViewController.setSubject("제목을 작성해주세요")
+            composeViewController.setMessageBody("내용을 작성해주세요", isHTML: false)
+            
+            present(composeViewController, animated: true)
+        } else {
+            presentAlert(title: "메일 앱을 사용할 수 없어요")
+        }
+    }
 }
 
 // MARK: - UITableView Protocol
@@ -98,7 +113,7 @@ extension SettingViewController: UITableViewDelegate {
         case 0, 2, 3:
             pushToNotion()
         case 1:
-            print("문의하기")
+            sendMail()
         default: return
         }
     }
@@ -143,5 +158,27 @@ extension SettingViewController: UITableViewDataSource {
 extension SettingViewController: SettingHeaderViewDelegate {
     func touchUpButton() {
         transition(SettingNameController(), transitionStyle: .presentFullScreen)
+    }
+}
+
+// MARK: - MFMailComposeViewControllerDelegate
+
+extension SettingViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result {
+        case .sent:
+            // 메일 발송 성공 ( 인터넷이 안되는 경우도 sent처리되고, 인터넷이 연결되면 메일이 발송됨. )
+            print("메일 발송 성공")
+        case .saved:
+            // 메일 임시 저장
+            print("메일 임시 저장공")
+        case .cancelled:
+            // 메일 작성 취소
+            print("메일 작성 취소")
+        case .failed:
+            // 메일 발송 실패 (오류 발생)
+            print("메일 발송 실패")
+        }
+        dismiss(animated: true)
     }
 }
