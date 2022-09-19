2f93ad063037809146ccec01cfc74c60c353f0d3
so yeon <alicesysy@swu.ac.kr>
2022-09-19 21:59:07 +0900

    [#21] MapView Default AnnotationView 구현

---
+++ NiCarNaeCar/NiCarNaeCar/Screen/Main/View/DefaultAnnotationView.swift
@@ -0,0 +1,30 @@
+//
+//  DefaultAnnotationView.swift
+//  NiCarNaeCar
+//
+//  Created by 소연 on 2022/09/19.
+//
+
+import MapKit
+
+import NiCarNaeCar_Resource
+
+class DefaultAnnoationView: MKMarkerAnnotationView {
+
+    static let ReuseID = "defaultAnnotation"
+
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        clusteringIdentifier = "default"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForDisplay() {
+        super.prepareForDisplay()
+        displayPriority = .defaultHigh
+        markerTintColor = R.Color.black200
+    }
+}
