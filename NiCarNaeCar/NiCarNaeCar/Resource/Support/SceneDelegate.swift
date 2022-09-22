f9a920aa9dfff7357e8f2db4f5c0c870d744f6a5
so yeon <alicesysy@swu.ac.kr>
2022-09-22 21:40:20 +0900

    [#46] SceneDelegate 네트워크 코드 주석처리

--- NiCarNaeCar/NiCarNaeCar/Resource/Support/SceneDelegate.swift
+++ NiCarNaeCar/NiCarNaeCar/Resource/Support/SceneDelegate.swift
@@ -41,13 +41,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
         
-        NetworkConnectionStatus.shared.startMonitoring { isConnected in
-            if !isConnected {
+//        NetworkConnectionStatus.shared.startMonitoring { isConnected in
+//            if !isConnected {
 //                DispatchQueue.main.async {
 //                    self.window?.rootViewController?.presentNetworkAlert()
 //                }
-            }
-        }
+//            }
+//        }
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
