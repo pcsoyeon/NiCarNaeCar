b7f52b037cd0ebcfcc1fa8353641feb340b37c8f
so yeon <alicesysy@swu.ac.kr>
2022-09-29 09:06:44 +0900

    [#43] 네트워크 연결 상태에 따른 분기처리

--- NiCarNaeCar/NiCarNaeCar/Network/Reachability/DisconnectedView.swift
+++ NiCarNaeCar/NiCarNaeCar/Network/Reachability/DisconnectedView.swift
@@ -5,4 +5,96 @@
 //  Created by 소연 on 2022/09/29.
 //
 
-import Foundation
+import UIKit
+
+import NiCarNaeCar_Util
+import NiCarNaeCar_Resource
+
+import SnapKit
+import Then
+
+final class DisconnectedView: BaseView {
+    
+    // MARK: - Property
+    
+    private let firstLabel = UILabel().then {
+        $0.text = "니"
+        $0.textColor = R.Color.black200
+        $0.font = NiCarNaeCarFont.title0.font
+        $0.alpha = 0.5
+    }
+    
+    private let secondLabel = UILabel().then {
+        $0.text = "카"
+        $0.textColor = R.Color.green100
+        $0.font = NiCarNaeCarFont.title0.font
+        $0.alpha = 0.5
+    }
+    
+    private let thirdLabel = UILabel().then {
+        $0.text = "내"
+        $0.textColor = R.Color.black200
+        $0.font = NiCarNaeCarFont.title0.font
+        $0.alpha = 0.5
+    }
+    
+    private let fourthLabel = UILabel().then {
+        $0.text = "카"
+        $0.textColor = R.Color.blue100
+        $0.font = NiCarNaeCarFont.title0.font
+        $0.alpha = 0.5
+    }
+    
+    private var overlayView = UIView().then {
+        $0.backgroundColor = UIColor(white: 0, alpha: 0.85)
+    }
+    
+    private var descriptionLabel = UILabel().then {
+        $0.text = """
+                  네트워크 연결을 확인해주세요.
+                  와이파이 또는 셀룰러를 연결해주세요.
+                  """
+        $0.textColor = .white
+        $0.font = NiCarNaeCarFont.body1.font
+        $0.textAlignment = .center
+        $0.numberOfLines = 0
+    }
+    
+    // MARK: - UI Method
+    
+    override func configureUI() {
+        backgroundColor = R.Color.white
+    }
+    
+    override func setLayout() {
+        addSubviews(firstLabel, secondLabel, thirdLabel, fourthLabel, overlayView, descriptionLabel)
+        
+        firstLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(321)
+            make.leading.equalTo(self.safeAreaLayoutGuide).inset(32)
+        }
+
+        secondLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(303)
+            make.leading.equalTo(firstLabel.snp.trailing)
+        }
+
+        thirdLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(321)
+            make.leading.equalTo(secondLabel.snp.trailing)
+        }
+
+        fourthLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).inset(303)
+            make.leading.equalTo(thirdLabel.snp.trailing)
+        }
+        
+        overlayView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
+}
--- NiCarNaeCar/NiCarNaeCar/Network/Reachability/NetworkMonitor.swift
+++ NiCarNaeCar/NiCarNaeCar/Network/Reachability/NetworkMonitor.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Network
+
+final class NetworkMonitor {
+    private let queue = DispatchQueue.global(qos: .background)
+    private let monitor: NWPathMonitor
+    
+    init() {
+        monitor = NWPathMonitor()
+        dump(monitor)
+        print("------------")
+    }
+    
+    func startMonitoring(statusUpdateHandler: @escaping (NWPath.Status) -> Void) {
+        monitor.pathUpdateHandler = { path in
+            DispatchQueue.main.async {
+                statusUpdateHandler(path.status)
+            }
+        }
+        monitor.start(queue: queue)
+    }
+    
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+}
