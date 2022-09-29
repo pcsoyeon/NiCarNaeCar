52bf3cadd4842c61186fff87fb5a5e03cf9c5043
so yeon <alicesysy@swu.ac.kr>
2022-09-29 09:04:59 +0900

    [#43] 온보딩 이미지 contentMode 수정

---
+++ NiCarNaeCar/NiCarNaeCar/Network/Reachability/DisconnectedView.swift
@@ -0,0 +1,8 @@
+//
+//  DisconnectedView.swift
+//  NiCarNaeCar
+//
+//  Created by 소연 on 2022/09/29.
+//
+
+import Foundation
---
+++ NiCarNaeCar/NiCarNaeCar/Network/Reachability/NetworkMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkMonitor.swift
+//  NiCarNaeCar
+//
+//  Created by 소연 on 2022/09/29.
+//
+
+import Foundation
