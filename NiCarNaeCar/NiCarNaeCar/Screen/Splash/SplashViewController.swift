ca54e508b75974c1f52fc8953281cb9c1cefb282
so yeon <alicesysy@swu.ac.kr>
2022-09-30 10:10:21 +0900

    [#52] 업데이트 유도 로직 구현

--- NiCarNaeCar/NiCarNaeCar/Screen/Splash/SplashViewController.swift
+++ NiCarNaeCar/NiCarNaeCar/Screen/Splash/SplashViewController.swift
@@ -100,7 +100,11 @@ final class SplashViewController: BaseViewController {
             self.showLabel(self.secondLabel) {
                 self.showLabel(self.thirdLabel) {
                     self.showLabel(self.fourthLabel) {
-                        self.presentMain()
+                        if self.checkUpdateAvailable() {
+                            self.presentUpdateAlert()
+                        } else {
+                            self.presentMain()
+                        }
                     }
                 }
             }
@@ -114,5 +118,49 @@ final class SplashViewController: BaseViewController {
         } completion: { _ in
             completion()
         }
+    }
+    
+    private func checkUpdateAvailable() -> Bool {
+        // CFBundleShortVersionString - 릴리즈 혹은 bundle 의 버전.
+        guard let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
+              // CFBundleIdentifier - 앱의 bundle ID.
+              let bundleID = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String,
+              let url = URL(string: "http://itunes.apple.com/lookup?bundleId=" + bundleID),
+              let data = try? Data(contentsOf: url),
+              let jsonData = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any],
+              let results = jsonData["results"] as? [[String: Any]],
+              results.count > 0,
+              let appStoreVersion = results[0]["version"] as? String else { return false }
+        
+        let currentVersionArray = currentVersion.split(separator: ".").map { $0 }
+        let appStoreVersionArray = appStoreVersion.split(separator: ".").map { $0 }
+        
+        // ✅ [Major].[Minor].[Patch]
+        print("현재 버전: ", currentVersion, "앱 스토어 버전: ", appStoreVersion)
+        // ✅ 앞자리(Major)가 낮으면 업데이트
+        if currentVersionArray[0] < appStoreVersionArray[0] {
+            return true
+        } else {
+            // ✅ 중간자리(Minor) 역시 낮으면 업데이트
+//            return currentVersionArray[1] < appStoreVersionArray[1] ? true : false
+            return false
+        }
+    }
+    
+    private func presentUpdateAlert() {
+        let alertVC = UIAlertController(title: "업데이트", message: "업데이트가 필요합니다.", preferredStyle: .alert)
+        let alertAtion = UIAlertAction(title: "업데이트", style: .default) { _ in
+            // ✅ App store connet 앱의 일반 정보의 Apple ID 입력.
+            let appleID = APIKey.AppId
+            // ✅ URL Scheme 방식을 이용해서 앱스토어를 연결.
+            guard let url = URL(string: "itms-apps://itunes.apple.com/app/\(appleID)") else { return }
+            // ✅ canOpenURL(_:) - 앱이 URL Scheme 처리할 수 있는지 여부를 나타내는 Boolean 값을 리턴한다.
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            }
+        }
+        alertVC.addAction(alertAtion)
+        
+        present(alertVC, animated: true)
     }
 }
